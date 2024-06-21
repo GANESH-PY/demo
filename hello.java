dae915897ce05485ad7a3c0564ea8018aba6cfde
akganesh <65601890+GANESH-PY@users.noreply.github.com>
2024-06-21 11:53:40 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String args[]){
+
+        int a = 15;
+        System.out.println("New Line"+a);
+        System.out.println("Hello World!");
+    }
+}
